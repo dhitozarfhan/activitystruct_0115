dae6997685972bd51be1a14337662ec04cf48683
dhitozarfhan <dhitozarfhan@gmail.com>
2024-04-01 16:07:32 +0700

    getline

--- pertemuan6 ke 3/pertemuan6 ke 3.cpp
+++ pertemuan6 ke 3/pertemuan6 ke 3.cpp
@@ -12,3 +12,15 @@ struct mahasiswa {
 	DetailAlamat alamat;
 	int umur;
 };
+
+int main() {
+	mahasiswa mhs;
+	cout << "Nomor mahasiswa : ";
+	getline(cin, mhs.nim;
+	cout << "Nama Mahasiswa : ";
+	getline(cin, mhs.nama);
+
+
+	
+
+
